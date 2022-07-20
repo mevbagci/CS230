97b7adf2dd047696ef707b879d243eb416696341
Mevluet Bagci <bagci@em.uni-frankfurt.de>
2022-07-20 17:18:51 +0200

    update

--- main_ad.py
+++ main_ad.py
@@ -25,7 +25,7 @@ flags.DEFINE_integer("max_grad", 5, "Maximum gradient to clip")
 flags.DEFINE_integer("batch_size", 1024, "Size of batch") # IMPORTANT; the bigger the better
 
 flags.DEFINE_integer("ngram", 1, "Ngram feature when ensemble = False.")
-flags.DEFINE_float("decay_rate", 0.09, "Decay rate of learning rate")
+flags.DEFINE_float("decay_rate", 0.99, "Decay rate of learning rate")
 flags.DEFINE_float("decay_step", 100, "Decay step of learning rate")
 
 # Validation hyper parameters
@@ -58,7 +58,7 @@ flags.DEFINE_float("hidden_dropout", 0.5, "Dropout rate of hidden layer")
 flags.DEFINE_float("hidden_dropout_min", 0.3, "Minimum dropout rate of hidden layer")
 flags.DEFINE_float("hidden_dropout_max", 0.8, "Maximum dropout rate of hidden layer")
 
-flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of the optimzier") # ** IMPORTANT; Origin value is 0.01
+flags.DEFINE_float("learning_rate", 0.0035, "Learning rate of the optimzier") # ** IMPORTANT; Origin value is 0.01
 flags.DEFINE_float("learning_rate_min", 5e-3, "Minimum learning rate of the optimzier")
 flags.DEFINE_float("learning_rate_max", 5e-2, "Maximum learning rate of the optimzier")
 
